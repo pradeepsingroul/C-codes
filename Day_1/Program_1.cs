94a4f241f1be13dced98829d1b7ff4c414db833a
predeep kumar singroul <prasingroul4899@gmail.com>
2023-07-16 22:23:11 +0530

    Datatype in c#

---
+++ Day_1/Program_1.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Day_1
+{
+    public class Program_1
+    {
+        /*datatypes
+        int 
+        String
+        bool
+        double
+        char
+        */
+
+    }
+}
